9b60795cc9dc362a62e15147ff5d412c7397b87c
Pol-Erwan <polerwankite@gmail.com>
2019-10-10 15:06:06 +0200

    dark_trader

---
+++ lib/dark_trader.rb
@@ -0,0 +1,47 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+URL = "https://coinmarketcap.com/"
+
+def perform
+
+	page = nokogiri (URL)
+
+	list_symbol = page.xpath("//a[@class='currency-name-container link-secondary']")  #récup symbol
+	    #puts list_symbol
+	list_value = page.xpath("//a[@class='price']")                                    #récup valeur
+            #puts list_value
+        crypto_hash = Hash.new                                                            #créa new tabH
+        
+	crypto_hash = make_hash(list_symbol,list_value)                                   #récup tabH full
+            #puts crypto_hash
+	return
+end
+
+def nokogiri (path)
+	return Nokogiri::HTML(open(path)) 
+end
+
+def make_hash (list_symbol,list_value)
+
+        tempory_hash = Hash.new                                                           #créa new tabH
+	#puts tempory_hash
+        for a in 0..list_symbol.size-1 do                      #remplir temp h avec symb et value associé
+        #a = 0
+        #while a < list_symbol.size-1
+  
+		tempory_hash[list_symbol[a].text]=list_value[a].text     
+                #tab = []
+                #puts " valeur #{a}"
+                #tab = tempory_hash 
+                #puts tab []         
+            #a = a + 1
+	end
+          puts tempory_hash  
+        return tempory_hash                                                               #retour valeur H
+end
+
+perform
+
+#h = crypto.zip(list_value).to_h
