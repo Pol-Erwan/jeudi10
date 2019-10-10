a0ed910d8602d742297a74963e455aadf62957bc
Pol-Erwan <polerwankite@gmail.com>
2019-10-10 22:59:49 +0200

    exo2

--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -2,30 +2,83 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-URL = "https://www.annuaire-des-mairies.com/95/avernes.html"
-
 def perform
 
-	page = nokogiri (URL)
-        list_mail = Hash.new
-	list_mail = page.xpath("//a[contains(@href,'https://www.annuaire-des-mairies.com/95/avernes.html')]")
-        
-        #a[href*='://']	//a[contains(@href, '://')]
-        puts list_mail.text
-   
-          
-	return
+        page
+        townhall(page)
+        get_townhall_urls(page)
+        get_townhall_email
+        townhall = townhall(page)
+        townhall_email = get_townhall_email
+        hashage2(townhall_email,townhall)
 end
 
-def nokogiri (path)
-	return Nokogiri::HTML(open(path)) 
+def page
+	return page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+end
+
+def townhall(page)
+  townhall = page.xpath("//a[@class='lientxt']/text()")
+  #puts townhall
+  return townhall
+end
+
+def get_townhall_urls(page)
+  url = page.xpath("//a[@class='lientxt']")
+  tableau = []
+  url.each do |townhall_url|
+      tableau << "http://annuaire-des-mairies.com/" + townhall_url['href']
+  end
+  #puts tableau
+  return tableau #je met tout les mail dans un tableau
 end
 
 
-def get_townhall_email(townhall_url)
-
+def get_townhall_email
+  i=0
+  table = get_townhall_urls(page)
+  townhall_email = []
+  while i < table.length
+    page2 = Nokogiri::HTML(open(table[i]))# ouverture d'une nouvelle page pour chaque élément du tableau
+    townhall_email << page2.xpath("/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]/text()")
+    i += 1
+  end
+  #puts townhall_email
+  return townhall_email
 end
 
+def hashage(townhall_email,townhall)# je créé un hash alimentant un tableau
+  i = 0
+  email = townhall_email
+  nom_ville = townhall
+  tableau_ville_mail = []
+  
+  while i < get_townhall_email.length
+    nouveau_hash = Hash.new()
+    nouveau_hash[tableau_ville_mail[i].text] = email[i].text
+    tableau_ville_mail << nouveau_hash
+    i += 1
+  end
+  puts tableau_ville_mail
+  return tableau_ville_mail
+end
+
+
+def hashage2 (townhall_email,townhall)
+
+        tempory_hash = Hash.new                                                           #créa new tabH
+	
+        for a in 0..townhall.size-1 do                      
+                temp_hash = Hash.new
+                tempory_hash[townhall[a].text]=townhall_email[a].text                     
+                tempory_hash.each do |temp_hash|
+                  print "{ #{temp_hash [0]}"
+                  puts " => #{temp_hash [1]} },"
+                end  
+	end
+            
+        return tempory_hash                                                               #retour valeur H
+end
 
 perform
 
